e03ab926ee462f7e8be6a1d03de397e0e7812fd8
stenman <roger.stenman@gmail.com>
2012-11-05 00:51:58 +0100

    Added basic view and basic logic

--- src/se/gildur/android/randomassignments/MainActivity.java
+++ src/se/gildur/android/randomassignments/MainActivity.java
@@ -1,8 +1,16 @@
 package se.gildur.android.randomassignments;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +18,41 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ListView namesListView = (ListView)findViewById(R.id.listView1);
+        
+        final EditText inputName = (EditText)findViewById(R.id.editText1);
+        
+        final ArrayList<String> names1 = new ArrayList<String>();
+        
+        final ArrayAdapter<String> aa;
+        
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names1);
+        
+        namesListView.setAdapter(aa);
+        
+        Button btnAddNameToList = (Button) findViewById(R.id.button1);
+        
+        btnAddNameToList.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				names1.add(0, inputName.getText().toString());
+				aa.notifyDataSetChanged();
+				inputName.setText("");
+			}
+		});
+        
+        Button btnAssignPresents = (Button) findViewById(R.id.button2);
+        
+        btnAssignPresents.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				//TODO: lägg in logik för assignment...
+			}
+		});
+        
     }
 
     @Override
