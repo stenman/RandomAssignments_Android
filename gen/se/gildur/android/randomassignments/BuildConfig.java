904ce54e0333e107a9036578cf721e73dd0c7007
stenman <roger.stenman@gmail.com>
2012-11-04 12:53:43 +0100

    First commit

---
+++ gen/se/gildur/android/randomassignments/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package se.gildur.android.randomassignments;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
